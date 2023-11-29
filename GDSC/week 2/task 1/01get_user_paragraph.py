bc660207528fe1403192c03e9f9c16d3f9c5ed51
Hailu Gashaw <122692092+haile27@users.noreply.github.com>
2023-11-29 09:03:17 +0300

    Update 01get_user_paragraph.py

--- GDSC/week 2/task 1/01get_user_paragraph.py
+++ GDSC/week 2/task 1/01get_user_paragraph.py
@@ -1,3 +1,5 @@
+'''#Write a function that prompts the user to enter a paragraph of text.
+  Store the input text in a variable for further processing.'''
 def get_user_paragraph():
     paragraph = input("Enter a paragraph of text: ")
     return paragraph
